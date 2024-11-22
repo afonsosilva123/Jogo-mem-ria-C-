ffb95ceb5f303981df3df82290c03e07f1dc8c21
afonsosilva123 <07afonsosilva@gmail.com>
2024-11-22 15:59:35 +0000

    Add files via upload

---
+++ Jogo da Memória/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Jogo_da_Memória
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.jogarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saírToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Green;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jogarToolStripMenuItem,
+            this.saírToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(825, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(312, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jogo da Memória";
+            // 
+            // jogarToolStripMenuItem
+            // 
+            this.jogarToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.jogarToolStripMenuItem.Name = "jogarToolStripMenuItem";
+            this.jogarToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.jogarToolStripMenuItem.Text = "Jogar";
+            this.jogarToolStripMenuItem.Click += new System.EventHandler(this.jogarToolStripMenuItem_Click);
+            // 
+            // saírToolStripMenuItem
+            // 
+            this.saírToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.saírToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.saírToolStripMenuItem.Name = "saírToolStripMenuItem";
+            this.saírToolStripMenuItem.Size = new System.Drawing.Size(48, 24);
+            this.saírToolStripMenuItem.Text = "Saír";
+            this.saírToolStripMenuItem.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.saírToolStripMenuItem.Click += new System.EventHandler(this.saírToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quantos Ícones?";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(431, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(95, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(825, 571);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "h";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem jogarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saírToolStripMenuItem;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Jogo da Memória/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_da_Memória
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void saírToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        List <int> xeirameafotos = new List <int>();
+        int ordemdalistaxeirameafotosmatate=0;
+
+        Random rnd = new Random();
+        int marcador=1;
+        int nomerofotos = 0;
+
+        private async void criadeiradebotoes ()
+        {
+            marcador = 1;
+            int nfotos = int.Parse (comboBox1.Text);
+            nomerofotos = nfotos * (nfotos - 1) / 2;
+
+            for (int i=0; i<nfotos; i++)
+            {
+                for (int k=0; k<nfotos-1; k++)
+                {
+                    Button butoooes = new Button();
+                    butoooes.Size = new Size(110, 90);
+                    butoooes.Location = new Point(i*110+35, k*110+30);
+                    butoooes.Tag = ordemdalistaxeirameafotosmatate;
+                    butoooes.BackgroundImageLayout= ImageLayout.Stretch; 
+                    ordemdalistaxeirameafotosmatate++;
+
+                    int x = 0;
+                    while (true) 
+                    { 
+                        x= rnd.Next(1,nomerofotos+1);
+                        if (xeirameafotos.Count(n=>n==x)!=2)
+                        {
+                            xeirameafotos.Add(x);
+                            break;
+                        }
+                    }
+                    if (x == 1) butoooes.BackgroundImage = Properties.Resources.india;
+                    else if (x == 2) butoooes.BackgroundImage = Properties.Resources.chad_2;
+                    else if (x == 3) butoooes.BackgroundImage = Properties.Resources.dhakla;
+                    else if (x == 4) butoooes.BackgroundImage = Properties.Resources.nigeria;
+                    else if (x == 5) butoooes.BackgroundImage = Properties.Resources.moscovo;
+                    else if (x == 6) butoooes.BackgroundImage = Properties.Resources.mexico_2;
+                    else if (x == 7) butoooes.BackgroundImage = Properties.Resources.mexico;
+                    else if (x == 8) butoooes.BackgroundImage = Properties.Resources._911;
+                    else if (x == 9) butoooes.BackgroundImage = Properties.Resources.texas;
+                    else if (x == 10) butoooes.BackgroundImage = Properties.Resources.missouri;
+
+                    butoooes.Click += new EventHandler(this.jogo);
+                    Controls.Add(butoooes);
+                }
+            }
+            await Task.Delay(2000);
+            foreach (Control control in Controls)
+            {
+                if (control is Button button)
+                {
+                    control.BackgroundImage = null;
+                }
+            }
+
+            marcador = 0;
+        }
+
+        private void jogarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (label1.Visible)
+            {
+                comboBox1.Hide();
+                label1.Hide();
+                label2.Hide();
+                criadeiradebotoes();
+            }
+            else
+            {
+                restart();
+            }
+         }
+
+        int peter=0, griffin=0, louis=0;
+        private async void jogo (object sender, EventArgs e)
+        {
+            if (marcador == 0)
+            {
+                marcador=1;
+                Button btn = (Button)sender;
+
+                griffin++;
+
+                int x = xeirameafotos[(int)btn.Tag];
+
+                if (x == 1) btn.BackgroundImage = Properties.Resources.india;
+                else if (x == 2) btn.BackgroundImage =  Properties.Resources.chad_2;
+                else if (x == 3) btn.BackgroundImage =  Properties.Resources.dhakla;
+                else if (x == 4) btn.BackgroundImage =  Properties.Resources.nigeria;
+                else if (x == 5) btn.BackgroundImage =  Properties.Resources.moscovo;
+                else if (x == 6) btn.BackgroundImage =  Properties.Resources.mexico_2;
+                else if (x == 7) btn.BackgroundImage =  Properties.Resources.mexico;
+                else if (x == 8) btn.BackgroundImage =  Properties.Resources._911;
+                else if (x == 9) btn.BackgroundImage =  Properties.Resources.texas;
+                else if (x == 10) btn.BackgroundImage = Properties.Resources.missouri;
+
+                if (griffin == 1)
+                {
+                    peter = (int)btn.Tag;
+                }
+                else if (griffin == 2)
+                {
+                    griffin = 0;
+                    if (x == xeirameafotos[peter])
+                    {
+                        btn.Click -= new EventHandler(this.jogo);
+
+                        foreach (Control control in Controls)
+                        {
+                            if (control.Tag != null && (int)control.Tag == peter)
+                            {
+                                control.Click -= new EventHandler(this.jogo);
+                                break;
+                            }
+                        }
+                        louis++;
+                        if (louis == nomerofotos)
+                        {
+                            MessageBox.Show("Ganhou");
+                            await Task.Delay(500);
+                            restart();
+                        }
+                    }
+                    else
+                    {
+                        await Task.Delay(400);
+                        btn.BackgroundImage = null;
+
+                        foreach (Control control in Controls)
+                        {
+                            if (control.Tag != null && (int)control.Tag == peter)
+                            {
+                                control.BackgroundImage = null;
+                                break;
+                            }
+                        }
+                    }
+                    peter = 0;
+                }
+                marcador = 0;
+            }
+        }
+        
+        private void restart ()
+        {
+            List<Control> buttonsToRemove = new List<Control>();
+
+            foreach (Control control in Controls)
+            {
+                if (control is Button)
+                {
+                    buttonsToRemove.Add(control);
+                }
+            }
+
+            foreach (Control button in buttonsToRemove)
+            {
+                Controls.Remove(button);
+            }
+
+            label1.Show();
+            label2.Show();
+            comboBox1.Show();
+            marcador = 1;
+            peter = 0;
+            griffin = 0;
+            nomerofotos = 0;
+            ordemdalistaxeirameafotosmatate = 0;
+            xeirameafotos.Clear();
+        }
+    }
+}
